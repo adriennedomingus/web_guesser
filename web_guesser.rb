059348f07667dd528d56e251bd7f50383b47b585
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-01-22 16:16:02 -0700

    guess limit

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,33 +1,48 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = Random.rand(101)
+@@secret_number = Random.rand(101)
+@@guesses = 5
+
 get '/' do
   guess = params["guess"].to_i
   message = check_guess(guess)
   color = set_color(message)
-  erb :index, :locals => {:number => SECRET_NUMBER, :guess => guess, :message => message, :color => color}
+  erb :index, :locals => {:number => @@secret_number, :guess => guess, :message => message, :color => color}
 end
 
 def check_guess(guess)
-  if guess > SECRET_NUMBER && guess > SECRET_NUMBER + 5
-    "Way too high!"
-  elsif guess < SECRET_NUMBER && guess < SECRET_NUMBER - 5
-    "Way too low!"
-  elsif guess < SECRET_NUMBER
-    "Too low!"
-  elsif guess > SECRET_NUMBER
-    "Too high!"
-  elsif guess == SECRET_NUMBER
-    "Correct! The secret number is #{SECRET_NUMBER}."
+  @@guesses -= 1
+  if @@guesses == 0
+    @@guesses = 5
+    @@secret_number = Random.rand(101)
+    "Sorry, you used all your guesses. The secret number has been reset. Try again. You have 5 guesses."
+  else
+    if guess == 0
+      "Guess a number between 1 and 100. You have 5 guesses to get it right."
+    elsif guess > @@secret_number && guess > @@secret_number + 5
+      "Way too high!\n You have #{@@guesses} guesses left."
+    elsif guess < @@secret_number && guess < @@secret_number - 5
+      "Way too low!\n You have #{@@guesses} guesses left."
+    elsif guess < @@secret_number
+      "Too low!\n You have #{@@guesses} guesses left."
+    elsif guess > @@secret_number
+      "Too high!\n You have #{@@guesses} guesses left."
+    elsif guess == @@secret_number
+      @@guesses = 5
+      @@secret_number = Random.rand(101)
+      "Correct! The secret number has been reset - play again! You have 5 guesses."
+    end
   end
 end
 
 def set_color(message)
-  if message == "Way too high!" || message == "Way too low!"
+  if message.start_with?("Way")
     "red"
-  elsif message == "Too low!" || message == "Too high!"
+  elsif message.start_with?("Too")
     "#f98989"
+  elsif message == "Guess a number between 1 and 100. You have 5 guesses to get it right."
+    "white"
   else
     "green"
   end
