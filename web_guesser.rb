01b19b64d37cfc0d1faf420faf89c44c387cd1b9
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-01-22 12:59:08 -0700

    background color changes based on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,19 +1,34 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = Random.rand(101)
+SECRET_NUMBER = Random.rand(101)
 get '/' do
   guess = params["guess"].to_i
-  if guess > number && guess > number + 5
-    message = "Way too high!"
-  elsif guess < number && guess < number - 5
-    message = "Way too low!"
-  elsif guess < number
-    message = "Too low!"
-  elsif guess > number
-    message = "Too high!"
-  elsif guess == number
-    message = "Correct! The secret number is #{number}"
+  message = check_guess(guess)
+  color = set_color(message)
+  erb :index, :locals => {:number => SECRET_NUMBER, :guess => guess, :message => message, :color => color}
+end
+
+def check_guess(guess)
+  if guess > SECRET_NUMBER && guess > SECRET_NUMBER + 5
+    "Way too high!"
+  elsif guess < SECRET_NUMBER && guess < SECRET_NUMBER - 5
+    "Way too low!"
+  elsif guess < SECRET_NUMBER
+    "Too low!"
+  elsif guess > SECRET_NUMBER
+    "Too high!"
+  elsif guess == SECRET_NUMBER
+    "Correct! The secret number is #{SECRET_NUMBER}."
   end
-  erb :index, :locals => {:number => number, :guess => guess, :message => message}
+end
+
+def set_color(message)
+  if message == "Way too high!" || message == "Way too low!"
+    "red"
+  elsif message == "Too low!" || message == "Too high!"
+    "#f98989"
+  else
+    "green"
+  end
 end
